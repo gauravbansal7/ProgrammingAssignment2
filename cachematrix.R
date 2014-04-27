958a45fd21d601be0445a14e100a7807b01a4780
Gaurav Bansal <gauravbansal7@gmail.com>
2014-04-27 15:40:04 +0530

    Code to cache the inverse of the matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculate and return the inverse of an matrix and caches it automatically
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())
+{
+    m1 <- NULL
+    setMat <- function(y)
+    {
+        x <<- y
+        m1 <<- NULL
+    }
+    getMat <- function() x
+    setInvMatrix <- function(mat) m1 <<- mat
+    getInvMatrix <- function() m1
+    cacheSolve(list(setMat = setMat, getMat = getMat, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix))
 }
 
+## Calculates the inverse of an matrix passed on through the 'makeCacheMatrix' and caches it automatically
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...)
+{
+    ## Return a matrix that is the inverse of 'x'
+    invMatrix <- x$getInvMatrix()
+    if(!is.null(invMatrix))
+    {
+        message("getting cached data")
+        return(invMatrix)
+    }
+    data <- x$getMat()
+    invMatrix <- solve(data, ...)
+    x$setInvMatrix(invMatrix)
+    invMatrix
 }
+
+## Test calls
+## x <- matrix(rnorm(100), 10, 10)
+## y <- makeCacheMatrix(x)
+## y
